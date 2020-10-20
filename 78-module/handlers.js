486341d7485e5a06502726ce6927b5e7fe7877e0
Aristotle S. Fabia <aristotle.fabia.s@gmail.com>
2020-10-21 00:00:02 +0800

    Module update

---
+++ 78-module/handlers.js
@@ -0,0 +1,7 @@
+
+
+export async function handleButtonClick(event) {
+  const { localCurrency, default: currency } = await import('./currencies.js');
+  console.log(localCurrency, currency);
+
+}
---
+++ 83 - Security/nasty.js
@@ -0,0 +1,23 @@
+import { sanitize } from 'dompurify';
+
+const input = document.querySelector('[name="input"]');
+const output = document.querySelector('.output');
+const buttons = document.querySelectorAll('nav button');
+input.addEventListener('input', () => {
+    console.log(output.innerHTML);
+  const clean = sanitize(input.value, {
+    FORBID_ATTR: ['width', 'height', 'style'],
+   FORBID_TAGS: ['style'],
+  });
+  output.innerHTML = clean.replace(/\n/g, '<br>');
+    output.innerHTML = input.value;
+});
+
+// trigger an input even on page load
+input.dispatchEvent(new Event('input'));
+
+buttons.forEach(button =>
+  button.addEventListener('click', e => {
+    alert(e.currentTarget.textContent);
+  })
+);
